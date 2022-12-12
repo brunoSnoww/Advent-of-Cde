c75556fdd83381bd2f8368044880860bc9edc04b
Bruno Neves <bruno.neves@issuu.com>
2022-12-12 08:15:51 +0000

    day12

---
+++ AoC22/day12/part1.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+typedef pair<int, int> ii;
+vector<string> matrix;
+vector<int> dx = {0, 0, 1, -1};
+vector<int> dy = {1, -1, 0, 0};
+int xS, yS, xE, yE;
+int N, M;
+bool isValid(int x, int y) { return x >= 0 && x < N && y >= 0 && y < M; }
+vector<vector<int>> d;
+
+void bfs(const vector<string> &matrix)
+{
+    queue<ii> q;
+    q.push(ii(xS, yS));
+    while (!q.empty())
+    {
+        ii front = q.front();
+        q.pop();
+        int x = front.first, y = front.second;
+        for (int k = 0; k < 4; ++k)
+        {
+            int nx = x + dx[k], ny = y + dy[k];
+            if (isValid(nx, ny) && d[nx][ny] == -1)
+            {
+                int d1 = matrix[x][y] - 'a', d2 = matrix[nx][ny] - 'a';
+                if (d2 <= d1 || d2 == d1 + 1)
+                {
+                    d[nx][ny] = d[x][y] + 1;
+                    q.push(ii(nx, ny));
+                }
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    string input;
+    int row = 0;
+    while (!cin.eof())
+    {
+        getline(cin, input);
+        for (int i = 0; i < input.size(); ++i)
+        {
+            if (input[i] == 'S')
+            {
+                xS = row;
+                yS = i;
+                input[i] = 'a';
+            }
+            if (input[i] == 'E')
+            {
+                xE = row;
+                yE = i;
+                input[i] = 'z';
+            }
+        }
+        matrix.push_back(input);
+        row++;
+    }
+
+    N = matrix.size(), M = matrix[0].size();
+
+    d.resize(N, vector<int>(M, -1));
+    bfs(matrix);
+    cout << d[xE][yE] + 1 << endl;
+
+    return 0;
+}
